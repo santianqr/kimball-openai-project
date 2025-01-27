5a4a5f9270e7a876d446026f3c5233932defda5e
Santiago Andres Quintero Rodriguez <santianqr@gmail.com>
2025-01-26 21:13:31 -0500

    Add database configuration management and update requirements

--- test.py
+++ test.py
@@ -1,12 +1,22 @@
 import psycopg2
+import config
 from psycopg2.extras import RealDictCursor
 
-DB_NAME = 'postgres'
-DB_USER = 'postgres'
+conn = config.Config()
+db_conn = conn.get_database_config()
 
 try:
-    conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST, port=DB_PORT)
+    conn = psycopg2.connect(
+        dbname=db_conn["name"],
+        user=db_conn["user"],
+        password=db_conn["password"],
+        host=db_conn["host"],
+        port=db_conn["port"],
+    )
     print("Connected to the database")
 except Exception as e:
     print(e)
     print("Database not connected")
+finally:
+    conn.close()
+    print("Database connection closed")
